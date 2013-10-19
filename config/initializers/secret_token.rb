38b31ad41372e0302153d8b8610fea3d13c80742
Michael Sepcot <msepcot@tunein.com>
2013-10-18 17:11:44 -0700

    initial commit, blank app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+R13Team188::Application.config.secret_key_base = '544e94f79a1cea73097942c363931c8b4389e7ceb3598bcc0de0adda07dc22006ed5c0e0e127d437bf5ef114bce87c6075129539c45448e17b67bdc005865bfe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+R13Team188::Application.config.session_store :cookie_store, key: '_r13-team-188_session'
